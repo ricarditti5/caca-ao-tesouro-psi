7cfdf1a79ae69d388879784383588f9a1e80b576
Ricardo <ricarditti5@gmail.com>
2025-10-29 12:14:37 +0000

    addin all project

---
+++ Projeto Caça ao tesouro/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Projeto_Caça_ao_tesouro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn15 = new System.Windows.Forms.Button();
+            this.btn16 = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblTentativas = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(134, 34);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(48, 46);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "X";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(134, 86);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(48, 46);
+            this.btn5.TabIndex = 1;
+            this.btn5.Text = "X";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(134, 138);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(48, 46);
+            this.btn9.TabIndex = 2;
+            this.btn9.Text = "X";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(188, 34);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(48, 46);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "X";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(188, 86);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(48, 46);
+            this.btn6.TabIndex = 4;
+            this.btn6.Text = "X";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(188, 138);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(48, 46);
+            this.btn10.TabIndex = 5;
+            this.btn10.Text = "X";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(242, 138);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(48, 46);
+            this.btn11.TabIndex = 6;
+            this.btn11.Text = "X";
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.btn11_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(242, 86);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(48, 46);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "X";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(242, 34);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(48, 46);
+            this.btn3.TabIndex = 8;
+            this.btn3.Text = "X";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(296, 34);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(48, 46);
+            this.btn4.TabIndex = 9;
+            this.btn4.Text = "X";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(296, 86);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(48, 46);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "X";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(296, 138);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(48, 46);
+            this.btn12.TabIndex = 11;
+            this.btn12.Text = "X";
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.Location = new System.Drawing.Point(134, 190);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(48, 46);
+            this.btn13.TabIndex = 12;
+            this.btn13.Text = "X";
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.btn13_Click);
+            // 
+            // btn14
+            // 
+            this.btn14.Location = new System.Drawing.Point(188, 190);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(48, 46);
+            this.btn14.TabIndex = 13;
+            this.btn14.Text = "X";
+            this.btn14.UseVisualStyleBackColor = true;
+            this.btn14.Click += new System.EventHandler(this.btn14_Click);
+            // 
+            // btn15
+            // 
+            this.btn15.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn15.Location = new System.Drawing.Point(242, 190);
+            this.btn15.Name = "btn15";
+            this.btn15.Size = new System.Drawing.Size(48, 46);
+            this.btn15.TabIndex = 14;
+            this.btn15.Text = "X";
+            this.btn15.UseVisualStyleBackColor = false;
+            this.btn15.Click += new System.EventHandler(this.btn15_Click);
+            // 
+            // btn16
+            // 
+            this.btn16.Location = new System.Drawing.Point(296, 190);
+            this.btn16.Name = "btn16";
+            this.btn16.Size = new System.Drawing.Size(48, 46);
+            this.btn16.TabIndex = 15;
+            this.btn16.Text = "X";
+            this.btn16.UseVisualStyleBackColor = true;
+            this.btn16.Click += new System.EventHandler(this.btn16_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(145, 274);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(190, 27);
+            this.btnSair.TabIndex = 20;
+            this.btnSair.Text = "Abandonar o Jogo";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblTentativas
+            // 
+            this.lblTentativas.AutoSize = true;
+            this.lblTentativas.Location = new System.Drawing.Point(13, 66);
+            this.lblTentativas.Name = "lblTentativas";
+            this.lblTentativas.Size = new System.Drawing.Size(0, 13);
+            this.lblTentativas.TabIndex = 22;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(179, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 98);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 313);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTentativas);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btn16);
+            this.Controls.Add(this.btn15);
+            this.Controls.Add(this.btn14);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn15;
+        private System.Windows.Forms.Button btn16;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblTentativas;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Projeto Caça ao tesouro/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Caça_ao_tesouro
+{
+    public partial class Form1 : Form
+    {
+        public int tentativas;
+        public Form1()
+        {
+            InitializeComponent();
+            //lblTentativas.Text = "Tentativas: " + tentativas;
+        }
+        //public int tentativas = 0;
+        //Botao para sair do Form
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //Função para gerar numeros aleatorios
+        public int gerarNúmero()
+        {
+            Random randomNum = new Random();
+            int num = randomNum.Next(1, 20);
+            return num;
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn1.Text = gerarNúmero().ToString();
+                btn1.Click -= btn1_Click;
+            }
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn2.Text = gerarNúmero().ToString();
+                btn2.Click -= btn2_Click;
+            }
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn3.Text = gerarNúmero().ToString();
+                btn3.Click -= btn3_Click;
+            }
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn4.Text = gerarNúmero().ToString();
+                btn4.Click -= btn4_Click;
+            }
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn5.Text = gerarNúmero().ToString();
+                btn5.Click -= btn5_Click;
+            }
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn6.Text = gerarNúmero().ToString();
+                btn6.Click -= btn6_Click;
+            }
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn7.Text = gerarNúmero().ToString();
+                btn7.Click -= btn7_Click;
+            }
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn8.Text = gerarNúmero().ToString();
+                btn8.Click -= btn8_Click;
+            }
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn9.Text = gerarNúmero().ToString();
+                btn9.Click -= btn9_Click;
+            }
+
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn10.Text = gerarNúmero().ToString();
+                btn10.Click -= btn10_Click;
+            }
+
+        }
+
+        private void btn11_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn11.Text = gerarNúmero().ToString();
+                btn11.Click -= btn11_Click;
+            }
+
+        }
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn12.Text = gerarNúmero().ToString();
+                btn12.Click -= btn12_Click;
+            }
+
+        }
+
+        private void btn13_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                btn13.Text = gerarNúmero().ToString();
+                btn13.Click -= btn13_Click;
+            }
+
+        }
+
+        private void btn14_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+
+            }
+            else
+            {
+                btn14.Text = gerarNúmero().ToString();
+                btn14.Click -= btn14_Click;
+            }
+        }
+
+        private void btn15_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+
+            }
+            else
+            {
+                btn15.Text = gerarNúmero().ToString();
+                btn15.Click -= btn15_Click;
+            }
+
+        }
+
+        private void btn16_Click(object sender, EventArgs e)
+        {
+            if (gerarNúmero() <= 3 || gerarNúmero() >= 12)
+            {
+                //MessageBox.Show('Encontratse o tesouro');
+                pictureBox1.Visible = true;
+
+            }
+            else
+            {
+                btn16.Text = gerarNúmero().ToString();
+                btn16.Click -= btn16_Click;
+            }
+        }
+    }
+}
